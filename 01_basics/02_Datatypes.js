3aaa90614195e8cb7b6e6aab31f6da03b38c39a8
Shubham Vedi <shubhamnv2@gmail.com>
2024-07-21 11:21:31 +0000

    Datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // Treat all js code as a newer vesrion
+
+// alert("Hello") // We are using Nodejs not Browser 
+
+console.log(3
+    +3);            // code readbility that should be high
+
+console.log("Shubham");
+
+let Name = "shubham"
+let age = 18
+let isLoggedIn = true
+let state = null
+
+
+// number => 2 to power 53
+//bigint  => for long numbers
+// String => ""
+// Boolean => true/false
+//null     =>  standalone value / Empty 
+//undfiend => Value is not assigned
+//symbol   => unique 
+
+
+//object 
+
+console.log(typeof null); // object 
+console.log(typeof undefined); //undefined
