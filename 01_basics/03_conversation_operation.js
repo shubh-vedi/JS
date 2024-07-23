8fb1ed2f8f988609ad490872f6a09beacec66944
Shubham Vedi <shubhamnv2@gmail.com>
2024-07-23 11:30:12 +0000

    Operations

--- 01_basics/03_conversation_operation.js
+++ 01_basics/03_conversation_operation.js
@@ -23,5 +23,60 @@ let booleanIsLoggedIn = Boolean(isloggedIn)
 let SomeNumber = 33
 
 let stringNumber = String(SomeNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************** Operations *********************
+
+let value = 3
+let negValye = -value
+// console.log(negValye);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "Hello"
+let str2 = " Shubham"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
+
+
+
+
