c0fd2003ee42a4d07961651c5223ee3ac5836ec3
Shubham Vedi <shubhamnv2@gmail.com>
2024-07-23 11:05:54 +0000

    conversation of datatype

---
+++ 01_basics/03_conversation_operation.js
@@ -0,0 +1,27 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// "true"  => 1 ; false => 0
+
+let isloggedIn = "shubham"
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => True ; 0 => False
+// ""  => false 
+// "shubham" => True 
+
+let SomeNumber = 33
+
+let stringNumber = String(SomeNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
