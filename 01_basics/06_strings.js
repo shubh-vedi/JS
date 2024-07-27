377e97e5fe356d10dedd63777110c2e6a95d8bfd
Shubham Vedi <shubhamnv2@gmail.com>
2024-07-27 08:42:35 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,38 @@
+const name = "shubham"
+const repoCount = 50
+
+// console.log(name + repoCount + " value");
+
+// console.log(`Hello My name is ${name} and my repocount is ${repoCount}`)
+
+const gameName = new String("Shubham-NV-com")
+
+// console.log(gameName[0]);
+// console.log(gameName__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf("b"));
+
+
+
+// const newwString = gameName.substring(0,4)
+// console.log(newwString);
+
+// const AnotherString = gameName.substring(-7,2)
+// console.log(AnotherString);
+
+
+const newStringOne = "        shubham.         "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "htttps://shubham.com/shubham%20vedi"
+console.log(url);
+console.log(url.replace("%20","-"));
+console.log(url.includes('sundar'));
+
+
+console.log(gameName.split("-"));
+
