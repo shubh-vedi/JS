dbec780d812e64da9b0c60c8e5fec93b6406e0dc
Shubham Vedi <shubhamnv2@gmail.com>
2024-07-28 12:23:36 +0000

    nums and maths

---
+++ 01_basics/07_num_and_Maths.js
@@ -0,0 +1,38 @@
+const score = 400
+
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+
+const otherNumber = 123.8766
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ******************** MAths **************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.6));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(2,5,6,7,4));
+// console.log(Math.max(2,5,6,7,4));
+
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20 
+
+console.log(Math.floor(Math.random()* (min-max+1))+min);
+
